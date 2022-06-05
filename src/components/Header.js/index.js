0f34e884b8d4cd4453aada644fd93c672571d01d
KDash201 <KVonMadison@Yahoo.com>
2022-06-04 21:08:11 -0400

    Header reconstruction

--- src/components/Header.js/index.js
+++ src/components/Header.js/index.js
@@ -4,69 +4,32 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 import avi from "../../assets/images/avi.png";
 // import Nav from "../Nav";
 
-function Header(props) {
-  const {
-    tabOptions = [],
-    setCurrentTab,
-    currentTab,
-    contactSelected,
-    setContactSelected,
-  } = props;
+function Header({ currentTab, setCurrentTab }) {
+  // const {
+  //   tabOptions = [],
+  //   setCurrentTab,
+  //   currentTab,
+  //   contactSelected,
+  //   setContactSelected,
+  // } = props;
 
-  // useEffect(() => {
-  //   document.title = capitalizeFirstLetter(currentTab.name);
-  // }, [currentTab]);
+  // // useEffect(() => {
+  // //   document.title = capitalizeFirstLetter(currentTab.name);
+  // // }, [currentTab]);
 
   return (
-    <header className="d-flex">
-      <div>
-        <p> K-Von Madison</p>
-        <img
-          src={avi}
-          className="my-2"
-          style={{ width: "20%" }}
-          alt="profile"
-        />
-      </div>
-      <nav className="d-flex navBar justify-content-between flex-wrap">
-        <a href="#home" onClick={() => setContactSelected(false)}>
-          Home
-        </a>
-        <a href="#about" onClick={() => setCurrentTab(true)}>
-          About Me
-        </a>
-        <a href="#contact" onClick={() => setContactSelected(false)}>
-          Contact Me
-        </a>
-        <a href="#projects" onClick={() => setCurrentTab(true)}>
-          Projects
-        </a>
-
-        {tabOptions.map((tab) => (
-          <li
-            className={`mx-1 ${
-              currentTab.name === tab.name && !contactSelected && "navActive"
-            }`}
-            key={tab.name}
-          >
-            <span
-              onClick={() => {
-                setCurrentTab(tab);
-                setContactSelected(false);
-              }}
-            >
-              {capitalizeFirstLetter(tab.name)}
-            </span>
-          </li>
-        ))}
-      </nav>
-      {/* <Nav
-        tabOptions={tabOptions}
-        setCurrentTab={setCurrentTab}
-        currentTab={currentTab}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      /> */}
+    <header>
+      <a
+        id="title-name"
+        className={currentTab == "About" ? "active-page" : "inactive-page"}
+        title-name
+        href="#"
+        onClick={() => {
+          setCurrentTab("About");
+        }}
+      >
+        K-Von Madison
+      </a>
     </header>
   );
 }
